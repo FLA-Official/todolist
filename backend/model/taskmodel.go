3f7abd0b8ddb5e7bcf9d9939721fd49225fc3777
FLA-Official <flapon.official@gmail.com>
2026-02-14 21:36:59 +0600

    added dependency injection for loose-coupling

--- backend/model/taskmodel.go
+++ backend/model/taskmodel.go
@@ -5,52 +5,67 @@ import (
 	"time"
 )
 
-type TaskBox struct {
-	Id          int
-	Title       string
-	Description string
-	CreatedTime time.Time
-	EndDate     time.Time
-	Complete    bool
+type Task struct {
+	Id          int       `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	CreatedTime time.Time `json:"createdtime"`
+	EndDate     time.Time `json:"endtime"`
+	Complete    bool      `json:"complete"`
 }
 
-var task []*TaskBox
-
-func Get() []*TaskBox {
-	return task
+type TaskRepo interface {
+	List() []*Task
+	GetTaskByID(id int) (*Task, error)
+	CreateTask(title string, description string, endDate time.Time) error
+	Completed()
+	Delete(id int) error
 }
 
-func GetTaskByID(id int) *TaskBox {
-	for i := range task {
-		if task[i].Id == id {
-			return task[i]
+type taskRepo struct {
+	tasklist []*Task
+}
+
+func NewTaskRepo() TaskRepo {
+	repo := &taskRepo{}
+	return repo
+}
+
+func (t *taskRepo) List() []*Task {
+	return t.tasklist
+}
+
+func (t *taskRepo) GetTaskByID(id int) (*Task, error) {
+	for _, task := range t.tasklist {
+		if task.Id == id {
+			return task, nil
 		}
 	}
-	return nil
+	return nil, nil
 }
 
-func AddTask(title string, description string, endDate time.Time) error {
+func (t *taskRepo) CreateTask(title string, description string, endDate time.Time) error {
 	//adding ID
-	id := len(task) + 1
-	task[len(task)].Id = id
+	id := len(t.tasklist) + 1
+	t.tasklist[len(t.tasklist)].Id = id
 
 	//Adding Title is mandetory
 	if title == "" {
 		return errors.New("Please provdie valid title")
 	} else {
-		task[len(task)].Title = title
+		t.tasklist[len(t.tasklist)].Title = title
 	}
 
 	//Adding description is optional
 	if description == "" {
-		task[len(task)].Description = ""
+		t.tasklist[len(t.tasklist)].Description = ""
 	} else {
-		task[len(task)].Description = description
+		t.tasklist[len(t.tasklist)].Description = description
 	}
 
 	//Adding time and validation
 	currentTime := time.Now()
-	task[len(task)].CreatedTime = currentTime
+	t.tasklist[len(t.tasklist)].CreatedTime = currentTime
 
 	if endDate.IsZero() {
 		return errors.New("Please provdie a valid time to complete the task")
@@ -59,15 +74,40 @@ func AddTask(title string, description string, endDate time.Time) error {
 	} else if endDate.Before(currentTime) {
 		return errors.New("due date cannot be in the past")
 	} else {
-		task[len(task)].EndDate = endDate
+		t.tasklist[len(t.tasklist)].EndDate = endDate
 	}
 
 	//task by default false at start
-	task[len(task)].Complete = false
+	t.tasklist[len(t.tasklist)].Complete = false
 
 	return nil
 }
 
-func (t *TaskBox) Completed() {
-	t.Complete = true
+func (t *taskRepo) Update(utask Task) (*Task, error) {
+	for idx, task := range t.tasklist {
+		if task.Id == utask.Id {
+			t.tasklist[idx] = &utask
+		}
+	}
+
+	return &utask, nil
+}
+
+func (t *taskRepo) Delete(taskID int) error {
+	var tempList []*Task
+
+	for _, t := range t.tasklist {
+		if t.Id != taskID {
+			tempList = append(tempList, t)
+		}
+	}
+
+	t.tasklist = tempList
+
+	return nil
+
+}
+
+func (t *taskRepo) Completed() {
+
 }
